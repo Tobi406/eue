35627c06e760b1491c0251314b3d5faddaac8a9e
Tobi406 <tobim406@gmail.com>
2021-07-29 11:42:56 +0200

    Add d3-parliament definitions

--- d3-parliament-env.d.ts
+++ d3-parliament-env.d.ts
@@ -1,1 +1,28 @@
-declare module 'd3-parliament';
+declare module 'd3-parliament' {
+  interface EnterFunctions {
+    smallToBig(boolean): this,
+    fromCenter(boolean): this,
+  }
+  interface ExitFunctions {
+    bigToSmall(boolean): this,
+    toCenter(boolean): this,
+  }
+  interface UpdateFunction {
+    animate(boolean): this,
+  }
+  interface CallbackFunction {
+    event: Event,
+    data: object,
+  }
+  type EventTypes = "click" | "dblclick" | "mousedown" | "mouseenter" | "mouseleave" | "mousemove" | "mouseout" | "mouseover" | "mouseup" | "touchcancel" | "touchend" | "touchmove" | "touchstart";
+  interface ParliamentFunctions {
+    width(number): this,
+    height(number): this,
+    innerRadiusCoef(innerRadiusCoef: number): this,
+    enter: EnterFunctions,
+    exit: ExitFunctions,
+    update: UpdateFunctions,
+    on(type: EventTypes, callback: (callback: CallbackFunction) => void): void, 
+  };
+  export default function (d3: any): ParliamentFunctions & d3.SelectionFn;
+}
