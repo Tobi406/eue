21cc9fb3e4c313920f899e19a7dbe78c662b87e1
Tobi406 <tobim406@gmail.com>
2021-11-07 18:52:52 +0100

    Change getNutsCode to find member state NUTS code

--- src/modules/map/getNutsCode.ts
+++ src/modules/map/getNutsCode.ts
@@ -1,7 +1,12 @@
 import allNuts from "src/data/nuts/all.json";
 
 const getNutsCode = (taName: string) => {
-  return allNuts.find(({ NAME_LATN }) => NAME_LATN.split(' ').includes(taName))!.NUTS_ID;
+  return allNuts.find(({ NAME_LATN }) =>
+    NAME_LATN.split(' ').includes(taName)
+    || taName.split(' ').includes(NAME_LATN)
+    || NAME_LATN.startsWith(taName)
+    || taName.startsWith(NAME_LATN)
+  )?.NUTS_ID;
 };
 
 export default getNutsCode;
